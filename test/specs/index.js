1e03720af656de9e298fb81692f3d9f99f13b35a
Ari Porad <ari@porad.com>
2015-08-18 12:09:15 -0700

    test: Add tests for [release skip] and [skip release]
    Refs semantic-release/semantic-release#41

--- test/specs/index.js
+++ test/specs/index.js
@@ -3,6 +3,8 @@ const { test } = require('tap')
 const analyzer = require('../../dist')
 
 test('derive version number from commits', (t) => {
+  t.plan(5)
+
   t.test('no change', (tt) => {
     tt.plan(2)
 
@@ -56,6 +58,34 @@ test('derive version number from commits', (t) => {
     }, (err, type) => {
       tt.error(err)
       tt.is(type, 'major')
+    })
+  })
+
+  t.test('[release skip]', (tt) => {
+    tt.plan(2)
+
+    analyzer({}, {
+      commit: {
+        hash: '1234',
+        message: 'feat(something): even cooler feature\n[release skip]'
+      }
+    }, (err, type) => {
+      tt.error(err)
+      tt.is(type, 'rskip')
+    })
+  })
+
+  t.test('[skip release]', (tt) => {
+    tt.plan(2)
+
+    analyzer({}, {
+      commit: {
+        hash: '1234',
+        message: 'feat(something): even cooler feature\n[skip release]'
+      }
+    }, (err, type) => {
+      tt.error(err)
+      tt.is(type, 'rskip')
     })
   })
 
