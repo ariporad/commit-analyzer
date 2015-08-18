a1d4558635401b27cba95a5912d5e9f3e18470ed
Ari Porad <ari@porad.com>
2015-08-18 07:58:08 -0700

    chore(standard): Comply with standard

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const { parseRawCommit } = require('conventional-changelog/lib/git')
 module.exports = function (pluginConfig, {commit}, cb) {
   commit = parseRawCommit(`${commit.hash}\n${commit.message}`)
 
-  if (!commit)                return cb(null, null)
-  if (commit.breaks.length)   return cb(null, 'major')
+  if (!commit) return cb(null, null)
+  if (commit.breaks.length) return cb(null, 'major')
   if (commit.type === 'feat') return cb(null, 'minor')
-  if (commit.type === 'fix')  return cb(null, 'patch')
+  if (commit.type === 'fix') return cb(null, 'patch')
 
   cb(null, null)
 }
